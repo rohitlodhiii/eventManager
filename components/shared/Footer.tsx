0aaba19904eee52d012d68b87cd4b5035abcb7cb
rohitlodhiii <rohitlodhi0701@gmail.com>
2024-01-19 23:09:49 +0530

    sd

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className='border-t'>
       <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
         <Link href='/'>
-          <Image src="/assets/images/logo.svg" alt='logo' width={128} height={38} />
+          Evenz
         </Link>
         <p>2024 Evenz | All rights reserved.</p>
       </div>
