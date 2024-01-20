e7265b1aded338c8f2da4183654c7d01887ac0ea
rohitlodhiii <rohitlodhi0701@gmail.com>
2024-01-20 09:47:28 +0530

    mongodb

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose' ;
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = "mongodb+srv://rohitlodhi2154:123123123@cluster0.n0tkbuq.mongodb.net/?retryWrites=true&w=majority"
 let cached = (global as any).mongoose || {conn : null , promise : null};
 
 export const connectToDatabase = async () =>{
